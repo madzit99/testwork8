e1933cab894d849de3cd2e214285cd2ba6ce5c42
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-29 16:31:20 +0600

    Сделал  добавления цитат  с сервера в зависимости от выбранной категории

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div className="col-3 d-flex flex-column gap-2 pt-3">
       <NavLink to="/">Все цитаты</NavLink>
       {categories.map((category) => (
-        <NavLink key={category.id} to={`quotes/${category.id}`}>{category.title}</NavLink>
+        <NavLink key={category.id} to={`/quotes/${category.id}`}>{category.title}</NavLink>
       ))}
     </div>
   );
--- src/Containers/Home.tsx
+++ src/Containers/Home.tsx
@@ -2,29 +2,35 @@ import { useEffect, useState } from "react";
 import { QuotesType } from "../types";
 import axiosApi from "../axiosApi";
 import QuotesList from "../Components/QuotesList";
+import { useParams } from "react-router-dom";
 
 const Home = () => {
   const [quotes, setQuotes] = useState<QuotesType | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
+  const { category } = useParams();
+
+  const fetchData = async () => {
+    try {
+      if (category) {
+        const response =
+          await axiosApi.get(`quotes.json?orderBy="category"&equalTo="${category}"
+        `);
+        setQuotes(response.data);
+      } else {
         const response = await axiosApi.get("quotes.json");
         setQuotes(response.data);
-      } catch (e) {
-        console.log(e);
       }
-    };
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
     void fetchData();
-  }, []);
+  }, [category]);
 
   return (
-    <div>
-      {quotes ? (
-      <QuotesList quotes={quotes} />) : (
-        <h1>Нет цитат</h1>
-      )}
-    </div>
+    <div>{quotes ? <QuotesList quotes={quotes} /> : <h1>Нет цитат</h1>}</div>
   );
 };
 
