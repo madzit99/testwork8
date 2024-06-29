dfde5e8d6a7d4a2b19dcd19d7255f49d4e93f89c
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-29 14:00:34 +0600

    Сделал Toolbar и вывел его на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
+import Toolbar from './Components/Toolbar/Toolbar'
 
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <div className="app-container">
+      <header>
+        <Toolbar />
+      </header>
+      <main className="container">
+        <Routes>
+          
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App
---
+++ src/Components/Toolbar/Toolbar.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+const Toolbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark nav-main bg-primary">
+      <div className="container-fluid w-75 d-flex justify-content-between align-items-center">
+        <NavLink className="navbar-brand" to={"/"}>
+          Цитаты
+        </NavLink>
+        <div className="d-flex">
+          <NavLink className="nav-link text-white me-5" to="/posts">
+            Главная
+          </NavLink>
+          <NavLink className="nav-link text-white me-5" to="/new-post">
+            Добавить цитаты
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
